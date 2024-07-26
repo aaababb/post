013874eaa6bfc42da6e6cdc3374564096302ad58
Abdurahman <abdurahman@Noutbuk-Abdurahman.local>
2024-07-27 01:14:20 +0600

    asjhdxa

--- src/Home.jsx
+++ src/Home.jsx
@@ -24,15 +24,13 @@ function Home() {
     <>
       {!showVideo ? (
         <div>
-          <div className='text-center absolute mx-[80px] mt-[560px] font-bold' >
-            <div className='text-[25px] text-red-700 w-[370px]'>
-              <p>Кыргыз кино КАРЫНДАШ Толугу менен</p>
-            </div>
-          </div>
           <div onClick={handleShow} className='flex justify-center items-center lg:w-100% cursor-pointer'>
             <img className='absolute w-16 h-16' src='https://cdn-icons-png.freepik.com/512/120/120625.png'/>
             <img  src='https://static.kinoafisha.info/k/movie_posters/canvas/800x1200/upload/movie_posters/1/0/1/8378101/884482179086.jpg'/>
           </div>
+            <div className='text-center font-bold mx-auto text-[25px] text-red-700 w-[370px]'>
+              <p>Кыргыз кино КАРЫНДАШ Толугу менен</p>
+            </div>
         </div>
       ) : (
         <div className="video-container">
